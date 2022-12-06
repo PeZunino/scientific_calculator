2f607979ff585bf18d99e3418974e7e13a06d87f
PeZunino <pedrozsantos@hotmail.com>
2022-12-06 13:17:33 -0300

    validate input type

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from tkinter import *
 
+
 root = Tk()
 root.title('Scientific Calculator')
 
+root["bg"] = "black"
+
+width = 500
+height = 200
+root.resizable(False, False)
+root.geometry("%dx%d" % (width, height))
+
+
+f_number = 0
+math_type = ""
+equal_lts_button = False
 Font_tuple = ("Helvetica", 20, "bold")
 
-MainInput = Entry(root, width=35, borderwidth=5, font=Font_tuple)
-MainInput.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-
-global equal_lts_button
-equal_lts_button = False
 
 def handle_equal():
     second_number = MainInput.get()
@@ -37,6 +44,7 @@ def handle_divide_button():
     f_number = int(first_number)
 
     MainInput.delete(0, END)
+    global equal_lts_button
     equal_lts_button = False
 
 
@@ -49,6 +57,7 @@ def handle_multiply_button():
     f_number = int(first_number)
 
     MainInput.delete(0, END)
+    global equal_lts_button
     equal_lts_button = False
 
 
@@ -61,6 +70,7 @@ def handle_subtract_button():
     f_number = int(first_number)
 
     MainInput.delete(0, END)
+    global equal_lts_button
     equal_lts_button = False
 
 
@@ -73,6 +83,7 @@ def handle_add_button():
     f_number = int(first_number)
 
     MainInput.delete(0, END)
+    global equal_lts_button
     equal_lts_button = False
 
 
@@ -87,8 +98,15 @@ def handle_button_click(number):
     else:
         MainInput.insert(0, str(current_value) + str(number))
 
+
 def handle_clear_button():
     MainInput.delete(0, END)
+
+
+def validate_entry(s):
+    if s in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        return True
+    return False
 
 
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: handle_button_click(0), font=Font_tuple)
@@ -102,35 +120,38 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: handle_butto
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: handle_button_click(8), font=Font_tuple)
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: handle_button_click(9), font=Font_tuple)
 
-add_button = Button(root, text="   +   ", padx=40, pady=20, command=handle_add_button, font=Font_tuple)
-subtract_button = Button(root, text="   -   ", padx=42, pady=22, command=handle_subtract_button, font=Font_tuple)
-divide_button = Button(root, text="   /   ", padx=40, pady=20, command=handle_divide_button, font=Font_tuple)
-multiply_button = Button(root, text="   *   ", padx=41, pady=21, command=handle_multiply_button, font=Font_tuple)
-equal_button = Button(root, text="   =   ", padx=40, pady=20, command=handle_equal, font=Font_tuple)
+add_button = Button(root, text="   +   ", padx=55, pady=20, command=handle_add_button, font=Font_tuple)
+subtract_button = Button(root, text="    -   ", padx=55, pady=20, command=handle_subtract_button, font=Font_tuple)
+divide_button = Button(root, text="   /   ", padx=55, pady=20, command=handle_divide_button, font=Font_tuple)
+multiply_button = Button(root, text="    *   ", padx=55, pady=20, command=handle_multiply_button, font=Font_tuple)
+equal_button = Button(root, text="   =   ", padx=55, pady=20, command=handle_equal, font=Font_tuple)
 clear_button = Button(root, text="Clear", padx=35, pady=15, command=handle_clear_button, font=Font_tuple)
 
+vcm = (root.register(validate_entry), "%S")
+MainInput = Entry(root, width=35, borderwidth=0, highlightthickness=0, font=Font_tuple, bg="black", fg="white",
+                  validate='key', vcmd=vcm)
+
+MainInput.insert(0, '0')
 MainInput.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-clear_button.grid(row=0, column=3)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-multiply_button.grid(row=1, column=3)
+#clear_button.grid(row=0, column=3)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-subtract_button.grid(row=2, column=3)
+#button_7.grid(row=1, column=0)
+#button_8.grid(row=1, column=1)
+#button_9.grid(row=1, column=2)
+#multiply_button.grid(row=1, column=3)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-add_button.grid(row=3, column=3)
+#button_4.grid(row=2, column=0)
+#button_5.grid(row=2, column=1)
+#button_6.grid(row=2, column=2)
+#subtract_button.grid(row=2, column=3)
 
-button_0.grid(row=4, column=1)
-equal_button.grid(row=4, column=3)
+#button_1.grid(row=3, column=0)
+#button_2.grid(row=3, column=1)
+#button_3.grid(row=3, column=2)
+#add_button.grid(row=3, column=3)
 
-
-
+#button_0.grid(row=4, column=1)
+#equal_button.grid(row=4, column=3)
 
 root.mainloop()
